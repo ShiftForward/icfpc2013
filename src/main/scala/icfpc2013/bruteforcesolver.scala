1f14b7479a4f276973f91a2095d0a971401bdda9
Rui Gonçalves <ruippeixotog@gmail.com>
2013-08-10 01:40:09 +0100

    Extract common interface from solvers.

--- src/main/scala/icfpc2013/bruteforcesolver.scala
+++ src/main/scala/icfpc2013/bruteforcesolver.scala
@@ -5,7 +5,7 @@ import scala.util.Random
 import scala.collection.mutable.ListBuffer
 import spray.util._
 
-object BruteForceSolver {
+object BruteForceSolver extends Solver {
   def getInputs: List[String] = {
     val inputs = ListBuffer[String]()
 
@@ -18,21 +18,19 @@ object BruteForceSolver {
     inputs.toList
   }
 
-  def genSolveAndGuess(trainReq: TrainRequest = TrainRequest(Some(4), None)) {
-    val train = Client.train(trainReq).await
-    println("Problem ID is " + train.id + " - " + train.challenge)
-    val operators = Set(If0, Fold0, Not, Shl1, Shr1, Shr4, Shr16, And, Or, Xor, Plus)
-    val possibleExpressions = ProgramGenerator.getExpressions(trainReq.size.get - 1, operators, Set(Id("x")))
+  def solve(problemId: String, size: Int, ops: Set[Operator], inputId: Id) = {
+    val possibleExpressions = ProgramGenerator.getExpressions(size - 1, ops, Set(inputId))
     val inputs = getInputs
-    val outputs = Client.eval(EvalRequest(Some(train.id), None, inputs)).await.outputs.get.map(_.toLowerCase)
+    val response = Client.eval(EvalRequest(Some(problemId), None, inputs)).await
+    val outputs = response.outputs.get.map(_.toLowerCase)
 
     val res = possibleExpressions.dropWhile { expression =>
-      val program = Program(Id("x"), expression)
+      val program = Program(inputId, expression)
       val f = BvCompiler(program)
       val results = inputs.map(input => f(BvCompiler.hexToLong(input))).map(BvCompiler.longToHex)
       results != outputs
     }
 
-    println("Solution is " + Program(Id("x"), res.head))
+    res.headOption
   }
 }
