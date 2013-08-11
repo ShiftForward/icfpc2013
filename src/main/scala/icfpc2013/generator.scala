099f1ed9c6706a9abd73bf23bd9f7fc626f2df3c
Hugo Ferreira <hugo@shiftforward.eu>
2013-08-11 16:21:03 +0100

    Cleanup ifs

--- src/main/scala/icfpc2013/generator.scala
+++ src/main/scala/icfpc2013/generator.scala
@@ -48,18 +48,14 @@ object ProgramGenerator {
     } yield {
       if (operator == And && (expression1.staticValue == Some(0L) || expression2.staticValue == Some(0L)))
         Zero
-      else if ((operator == Or || operator == Xor || operator == Plus) && expression1.staticValue == Some(0L))
+      else if ((operator == Or || operator == Xor || operator == Plus || operator == Shl1 || operator == Shr4 || operator == Shr16) && expression1.staticValue == Some(0L))
         expression2
-      else if ((operator == Or || operator == Xor || operator == Plus) && expression2.staticValue == Some(0L))
+      else if ((operator == Or || operator == Xor || operator == Plus || operator == Shl1 || operator == Shr4 || operator == Shr16) && expression2.staticValue == Some(0L))
         expression1
       else if ((operator == And || operator == Or) && (expression1 == expression2 || expression1.isStaticallyEqualTo(expression2) ))
         expression1
       else if ((operator == Xor) && (expression1 == expression2 || expression1.isStaticallyEqualTo(expression2) ))
         Zero
-      else if ((operator == Shl1 || operator == Shr4 || operator == Shr16) && expression1.staticValue == Some(0L))
-        expression2
-      else if ((operator == Shl1 || operator == Shr4 || operator == Shr16) && expression2.staticValue == Some(0L))
-        expression1
       else
         Op2(operator, expression1, expression2)
     }
