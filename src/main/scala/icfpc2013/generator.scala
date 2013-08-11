bee0b1cdc488c0512e52438c616074f16811452f
Hugo Ferreira <hugo@shiftforward.eu>
2013-08-11 16:08:01 +0100

    Peephole more Xors

--- src/main/scala/icfpc2013/generator.scala
+++ src/main/scala/icfpc2013/generator.scala
@@ -48,9 +48,9 @@ object ProgramGenerator {
     } yield {
       if (operator == And && (expression1.staticValue == Some(0L) || expression2.staticValue == Some(0L)))
         Zero
-      else if (operator == Or && expression1.staticValue == Some(0L))
+      else if ((operator == Or || operator == Xor) && expression1.staticValue == Some(0L))
         expression2
-      else if (operator == Or && expression2.staticValue == Some(0L))
+      else if ((operator == Or || operator == Xor) && expression2.staticValue == Some(0L))
         expression1
       else if ((operator == And || operator == Or) && (expression1 == expression2 || expression1.isStaticallyEqualTo(expression2) ))
         expression1
